40b5b2a6d82de2f552a4631bf31d3c20bee04391
maeng-kim <ehseo0410@gmail.com>
2024-05-30 02:16:06 +0900

    fix: index.js수정정

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const http = require("http");
 const express = require('express');
+const client = require('./db'); //db.js불러와
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const cors = require('cors');
 const usersRouter = require('./routes/users');
-
 const app = express();
+
 
 // Body parser middleware
 app.use(bodyParser.json());
+// 미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/auth-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+// 뷰 엔진 설정
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
 
 // Routes
 app.use('/api/users', require('./routes/users'));
+const mandalartRoutes = require('./routes/mandalart');
+app.use('/mandalart', mandalartRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+//const server = http.createServer(app);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.redirect('/mandalart');
+});
