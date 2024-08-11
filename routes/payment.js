beced112e0107b0c3d354a8e4f13d0c7572c3b18
sen <5.22nhz@gmail.com>
2024-08-12 01:07:36 +0900

    Fix: 멤버십 결제

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const  client  = require('../db/db_connect'); // MySQL 클라이언트 사용
+const client = require('../db/db_connect'); // MySQL 클라이언트 사용
 const USER_COOKIE_KEY = 'USER';
 
 let got;
@@ -15,32 +15,38 @@ router.post("/confirm", async function (req, res) {
   const encryptedSecretKey =
     "Basic " + Buffer.from(widgetSecretKey + ":").toString("base64");
 
-    try {
-      const response = await got.post("https://api.tosspayments.com/v1/payments/confirm", {
-          headers: {
-              Authorization: encryptedSecretKey,
-              "Content-Type": "application/json",
-          },
-          json: {
-              orderId: orderId,
-              amount: amount,
-              paymentKey: paymentKey,
-          },
-          responseType: "json",
-      });
-      console.log(response.body);
-      res.status(response.statusCode).json(response.body);
+  try {
+    const response = await got.post("https://api.tosspayments.com/v1/payments/confirm", {
+      headers: {
+        Authorization: encryptedSecretKey,
+        "Content-Type": "application/json",
+      },
+      json: {
+        orderId: orderId,
+        amount: amount,
+        paymentKey: paymentKey,
+      },
+      responseType: "json",
+    });
+    console.log(response.body);
+    res.status(response.statusCode).json(response.body);
   } catch (error) {
-      if (error.response && error.response.body) {
-          // HTML을 포함한 에러 응답 처리
-          console.log("Error response body:", error.response.body);
-          res.status(error.response.statusCode || 500).send(error.response.body);
-      } else {
-          console.log("Unknown error:", error);
-          res.status(500).json({ message: "서버 오류" });
-      }
+    if (error.response) {
+      // HTML 포함
+      console.log("Error response body:", error.response.body);
+      res.status(error.response.statusCode || 500).send(error.response.body);
+    } else {
+      // response 포함 x
+      console.log("Error without response:", error);
+      res.status(500).json({
+        message: "서버 오류",
+        error: {
+          message: error.message,
+          stack: error.stack,
+        }
+      });
+    }
   }
-  
 });
 
 module.exports = router;
