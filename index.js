5b25f05a466cd5fe016308d54ef05a644cd126d6
sen <5.22nhz@gmail.com>
2024-08-12 00:35:59 +0900

    Fix: 멤버십 결제

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const successRouter = require('./routes/success'); // 추가
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // 설정
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
--- routes/payment.js
+++ routes/payment.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const  client  = require('../db/db_connect'); // MySQL 클라이언트 사용
 const USER_COOKIE_KEY = 'USER';
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 let got;
 (async () => {
