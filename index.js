e6de51da83484b66a0fcf248e2adc888594444be
hyeumm <20221159@sungshin.ac.kr>
2024-08-12 17:45:08 +0900

    index복구 및 db 업데이트 조건 변경

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const successRouter = require('./routes/success'); // 추가
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
 app.use(cors());
@@ -131,8 +131,8 @@ app.get('/payment', async (req, res) => {
 });
 
 app.post('/payment', async(req, res) => {
-    if(res.locals.user) {
-        const userId = res.locals.user.user_id;
+    if(res.locals.users) {
+        const userId = res.locals.users.user_id;
         const { amount } = res.body; //결제금액.. 가튼 결제 관련 정보
         try {
             await savePaymentInfo(userId, amount); //결제 관련 정보를 데베에 저장 근데 토스로직을 잘 몰라서 일단 이렇게 둠
