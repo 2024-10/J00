de666bf62fbac60eade7cd92f02d83fd6ae4bea6
hyeumm <20221159@sungshin.ac.kr>
2024-08-12 19:51:53 +0900

    got 모듈 변경

--- routes/payment.js
+++ routes/payment.js
@@ -1,39 +1,56 @@
 const express = require('express');
 const router = express.Router();
-const  client  = require('../db/db_connect'); // MySQL 클라이언트 사용
+const https = require('https');
 const USER_COOKIE_KEY = 'USER';
 
-let got;
-(async () => {
-  got = (await import('got')).default;
-})();
+const widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
+const encryptedSecretKey =
+  "Basic " + Buffer.from(widgetSecretKey + ":").toString("base64");
 
 router.post("/confirm", async function (req, res) {
   const { paymentKey, orderId, amount } = req.body;
 
-  const widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
-  const encryptedSecretKey =
-    "Basic " + Buffer.from(widgetSecretKey + ":").toString("base64");
+  const postData = JSON.stringify({
+    orderId: orderId,
+    amount: amount,
+    paymentKey: paymentKey,
+  });
 
-  try {
-    const response = await got.post("https://api.tosspayments.com/v1/payments/confirm", {
-      headers: {
-        Authorization: encryptedSecretKey,
-        "Content-Type": "application/json",
-      },
-      json: {
-        orderId: orderId,
-        amount: amount,
-        paymentKey: paymentKey,
-      },
-      responseType: "json",
+  const options = {
+    hostname: 'api.tosspayments.com',
+    path: '/v1/payments/confirm',
+    method: 'POST',
+    headers: {
+      Authorization: encryptedSecretKey,
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length,
+    },
+  };
+
+  const request = https.request(options, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
     });
-    console.log(response.body);
-    res.status(response.statusCode).json(response.body);
-  } catch (error) {
-    console.log(error.response.body);
-    res.status(error.response.statusCode).json(error.response.body);
-  }
+
+    response.on('end', () => {
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        res.status(response.statusCode).json(JSON.parse(data));
+      } else {
+        res.status(response.statusCode).json(JSON.parse(data));
+      }
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error('Request error:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  });
+
+  request.write(postData);
+
+  request.end();
 });
 
 module.exports = router;
